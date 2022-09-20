d1feaf992b2621a60f2dadc3618640310a0b86e3
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2022-09-20 16:31:30 +0200

    Np 9319 pdf thumbnails (#11)
    * NP-9319-pdf-thumbnails, created test
    
    * NP-9319-pdf-thumbnails, implemented functionality.
    
    * NP-9319-pdf-thumbnails, code-review
    
    * NP-9319-pdf-thumbnails, code-review

--- thumbnail-service/src/test/java/no/sikt/nva/thumbnail/ThumbnailRequestHandlerTest.java
+++ thumbnail-service/src/test/java/no/sikt/nva/thumbnail/ThumbnailRequestHandlerTest.java
@@ -74,6 +74,8 @@ class ThumbnailRequestHandlerTest {
     private static final String QUICK_TIME_MOVIE_FILENAME = "quickTimeWindow.mov";
     private static final String QUICK_TIME_MIME_TYPE = "video/quicktime";
     private static final String MOVIE_PATH = "videos";
+    private static final String PDF_FILENAME = "wireframe.pdf";
+    private static final String PDF_MIMETYPE = "application/pdf";
     private TestAppender appender;
 
     private ThumbnailerInitializer thumbnailerInitializer;
@@ -90,17 +92,19 @@ class ThumbnailRequestHandlerTest {
     }
 
     @Test
+    public void shouldBeAbleToCreateThumbnailFromPdf() throws IOException {
+        var shouldHaveContentDisposition = false;
+        assertThumbnailGeneratedForFileFromResources(DOCUMENTS_PATH, PDF_FILENAME,
+                                                     PDF_MIMETYPE,
+                                                     shouldHaveContentDisposition);
+    }
+
+    @Test
     public void shouldBeAbleToConvertQuickTimeMovie() throws IOException {
-        var s3Path = randomS3Path();
-        var expectedThumbnailURL = craftExpectedURL(s3Path);
-        var s3Client = new FakeS3ClientWithPutObjectSupport(QUICK_TIME_MOVIE_FILENAME,
-                                                            MOVIE_PATH,
-                                                            QUICK_TIME_MIME_TYPE);
-        var s3Event = createNewFileUploadEvent(UnixPath.of(MOVIE_PATH, QUICK_TIME_MOVIE_FILENAME),
-                                               s3Client, s3Path);
-        var handler = new ThumbnailRequestHandler(s3Client, thumbnailerInitializer);
-        var thumbnailUrl = handler.handleRequest(s3Event, CONTEXT);
-        assertThat(thumbnailUrl, is(equalTo(expectedThumbnailURL)));
+        var shouldHaveContentDisposition = false;
+        assertThumbnailGeneratedForFileFromResources(MOVIE_PATH, QUICK_TIME_MOVIE_FILENAME,
+                                                     QUICK_TIME_MIME_TYPE,
+                                                     shouldHaveContentDisposition);
     }
 
     @Test
